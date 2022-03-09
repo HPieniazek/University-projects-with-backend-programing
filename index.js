8ba497fe25df6d3199cda4ff1efe20600d8bd16b
Hubert Pieniazek <hubert.pieniazek@microsoft.wsei.edu.pl>
2022-03-09 11:20:03 +0100

    koniec zajec

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('Hello');
+    
 });
-app.get('/', function (req, res) {
-    res.send(2+2) 
-    })
+app.param('name', function(req, res, next, name) {
+    const modified = name.toUpperCase();
+  
+    req.name = modified;
+    next();
+  });
+app.get('/api/users/:name', function(req, res) {
+    res.send('Hello ' + req.name + '!');
+  });
+
 app.listen(3000);
