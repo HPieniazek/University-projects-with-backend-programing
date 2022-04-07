1b70d8b027be365d07fa6a8a057e5bb87e5e9ab1
HPieniazek <hubert.pieniazek@microsoft.wsei.edu.pl>
2022-04-07 10:55:09 +0200

    clean

--- Projekt-2/src/Services/services.ts
+++ Projekt-2/src/Services/services.ts
@@ -2,11 +2,12 @@ import fs from 'fs';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken'
 import {Note, Tag, User} from '../classes';
+
 const notes = require('./Notes/notes');
 const tags = require('./Tags/tags');
 
-const storeNoteFile = '../Notes/dataNotesFile.json';
-const storeTagFile = '../Tags/dataTagsFile.json';
+const dataNotesFile = '../Notes/dataNotesFile.json';
+const dataTagsFile = '../Tags/dataTagsFile.json';
 
 export function checkToken(req: Request){
       try{
@@ -23,32 +24,31 @@ class Service {
       public async updateNoteStorage(): Promise<void> {
         const data = { notes };
         try {
-          await fs.promises.writeFile(storeNoteFile, JSON.stringify(data));
+          await fs.promises.writeFile(dataNotesFile, JSON.stringify(data));
         } catch (err) {
           console.log(err);
         }
       }
       public async readNoteStorage(): Promise<void> {
         try {
-          const data = await fs.promises.readFile(storeNoteFile, 'utf-8');
-          notes = JSON.parse(data).notes;
+          const data = await fs.promises.readFile(dataNotesFile, 'utf-8');
+          let noteData = JSON.parse(data).notes;
         } catch (err) {
           console.log(err);
         }
       }
       public async updateTagStorage(): Promise<void> {
-        // to do: parametry
-        const data = { tags };
+        const data =  tags ;
         try {
-          await fs.promises.writeFile(storeTagFile, JSON.stringify(data));
+          await fs.promises.writeFile(dataTagsFile, JSON.stringify(data));
         } catch (err) {
           console.log(err);
         }
       }
       public async readTagStorage(): Promise<void> {
         try {
-          const data = await fs.promises.readFile(storeTagFile, 'utf-8');
-          tags = JSON.parse(data).tags;
+          const data = await fs.promises.readFile(dataTagsFile, 'utf-8');
+          let tagsData = JSON.parse(data).tags;
         } catch (err) {
           console.log(err);
         }
