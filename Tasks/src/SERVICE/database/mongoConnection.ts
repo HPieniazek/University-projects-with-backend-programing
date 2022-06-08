2e662a3056522e7e270c16efc98fc545a1925a3e
HPieniazek <pieniazekh@gmail.com>
2022-06-08 09:48:28 +0200

    .

--- Tasks/src/SERVICE/database/mongoConnection.ts
+++ Tasks/src/SERVICE/database/mongoConnection.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { error } from 'console';
-//import { Tag } from '../../MODEL/classes';
 
 
 const app = express();
